3d864147b404f488569f3d20b8d84970fab5cddc
Demarion123 <163315193+Demarion123@users.noreply.github.com>
2024-03-25 08:06:05 -0700

    Update calculation_with_cmd.py

--- calculatiion_with_cmd/calculation_with_cmd.py
+++ calculatiion_with_cmd/calculation_with_cmd.py
@@ -1,16 +1,19 @@
-
-print("input numbers that will be calculated")
-
-while True:
-    a = int(input("input the first number: "))
-    b = int(input("input the second number: "))
-    choice = input("choose from addition,subtraction,multiplication and division[ +,-,*,/]: ")
-    if choice == "+":
-        print( a + b )
-    elif choice == "-":
-        print( a - b )
-    elif choice == "/":
-        print( a / b )
-    else:
-        print( a * b )
-        continue
+print("Welcome to My calculator")
+answer = input("Press any key to commence calculation.If you wish to quit click [q]. ").lower()
+if answer == "q":
+    print("Goodbye!")
+    quit
+else:
+    print("Let's go")
+    while True:
+        a = int(input("input the first number: "))
+        b = int(input("input the second number: "))
+        choice = input("choose from addition,subtraction,multiplication and division[+,-,*,/]: ").lower()
+        if choice == "+":
+            print( a + b )
+        elif choice == "-":
+            print( a - b )
+        elif choice == "/":
+            print( a / b )
+        else:
+            print( a * b )    
